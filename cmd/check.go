65c8d5eee85b26adc2c9134446fa88aecf393189
zhangyuan <zhangyuan@users.noreply.github.com>
2024-06-22 22:27:15 +0800

    refactor

--- cmd/check.go
+++ cmd/check.go
@@ -89,34 +89,55 @@ var generateQueryCmd = &cobra.Command{
 			log.Fatalln(err)
 		}
 
-		if returnSingleQuery {
-			query, err := compiler.ToQuery(spec)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			fmt.Println(query)
-			return
+		query, err := compiler.ToQuery(spec)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(query)
+	},
+}
 
-		} else {
-			statements, err := compiler.ToQueries(spec)
-			if err != nil {
-				log.Fatalln(err)
-			}
+var generateQueriesCmd = &cobra.Command{
+	Use:   "generate-queries",
+	Short: "Generate sql queries",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("Error loading .env file")
+		}
 
-			for idx, sql := range statements {
-				fmt.Println(sql)
-				if idx != len(statements)-1 {
-					fmt.Println("=======")
-				}
+		dsn := os.Getenv("DSN")
+
+		spec, err := v2.ParseSpec(specPath)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		adapter, err := adapters.NewAdapterFromDSN(dsn)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		compiler := v2.NewCompiler(adapter)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		statements, err := compiler.ToQueries(spec)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for idx, sql := range statements {
+			fmt.Println(sql)
+			if idx != len(statements)-1 {
+				fmt.Println("----------")
 			}
-			return
 		}
 	},
 }
 
 var specPath string
 var format string
-var returnSingleQuery bool
 
 func init() {
 	rootCmd.AddCommand(queryCmd)
@@ -126,6 +147,9 @@ func init() {
 
 	rootCmd.AddCommand(generateQueryCmd)
 	generateQueryCmd.Flags().StringVarP(&specPath, "spec", "s", "dq.yaml", "Path to the rules file")
-	generateQueryCmd.Flags().BoolVar(&returnSingleQuery, "single-query", false, "Return a single query")
 	_ = generateQueryCmd.MarkFlagRequired("spec")
+
+	rootCmd.AddCommand(generateQueriesCmd)
+	generateQueriesCmd.Flags().StringVarP(&specPath, "spec", "s", "dq.yaml", "Path to the rules file")
+	_ = generateQueriesCmd.MarkFlagRequired("spec")
 }
